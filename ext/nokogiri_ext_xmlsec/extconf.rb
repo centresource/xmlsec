f95cc663cc3c8da0c3024f7550efbb61e3d4fab5
Jeremy Holland <jeremy@jeremypholland.com>
2013-09-17 12:11:10 -0500

    Attempting to discover why not building on heroku

--- ext/nokogiri_ext_xmlsec/extconf.rb
+++ ext/nokogiri_ext_xmlsec/extconf.rb
@@ -6,15 +6,12 @@ end
 
 barf unless have_header('ruby.h')
 
-if pkg_config('xmlsec1-openssl')
-  # HACK 'openssl' is escaped too many times, I don't know why
-  if $CFLAGS =~ /\-DXMLSEC_CRYPTO=\\\\\\"openssl\\\\\\"/
-    $CFLAGS['-DXMLSEC_CRYPTO=\\\\\\"openssl\\\\\\"'] =
-      '-DXMLSEC_CRYPTO=\\"openssl\\"'
-  end
-  
-  have_library 'xmlsec1-openssl'
-  create_makefile('nokogiri_ext_xmlsec')
-else
-  barf "xmlsec1 is not installed."
+pkg_config('xmlsec1-openssl')
+# HACK 'openssl' is escaped too many times, I don't know why
+if $CFLAGS =~ /\-DXMLSEC_CRYPTO=\\\\\\"openssl\\\\\\"/
+  $CFLAGS['-DXMLSEC_CRYPTO=\\\\\\"openssl\\\\\\"'] =
+    '-DXMLSEC_CRYPTO=\\"openssl\\"'
 end
+
+have_library 'xmlsec1-openssl'
+create_makefile('nokogiri_ext_xmlsec')
