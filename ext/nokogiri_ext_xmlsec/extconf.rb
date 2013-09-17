026f6c127c007e307707653c99a3835e87ad6aa1
Jeremy Holland <jeremy@jeremypholland.com>
2013-09-17 14:40:33 -0500

    Attempting to make it work

--- ext/nokogiri_ext_xmlsec/extconf.rb
+++ ext/nokogiri_ext_xmlsec/extconf.rb
@@ -6,12 +6,16 @@ end
 
 barf unless have_header('ruby.h')
 
-pkg_config('xmlsec1-openssl')
+unless pkg_config('xmlsec1-openssl')
+  pkg_config('xmlsec1')
+end
 # HACK 'openssl' is escaped too many times, I don't know why
 if $CFLAGS =~ /\-DXMLSEC_CRYPTO=\\\\\\"openssl\\\\\\"/
   $CFLAGS['-DXMLSEC_CRYPTO=\\\\\\"openssl\\\\\\"'] =
     '-DXMLSEC_CRYPTO=\\"openssl\\"'
 end
 
-have_library 'xmlsec1-openssl'
+unless have_library 'xmlsec1-openssl'
+  have_library 'xmlsec1'
+end
 create_makefile('nokogiri_ext_xmlsec')
